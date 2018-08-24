1f0e15f3aff458c355924b2b1ed2a789179dea67
akchoukri <achoukri@ymagisgroup.com>
2018-08-24 08:13:33 +0100

    creation projet

---
+++ src/main/java/com/ymagis/GestionDuMaterielApplication.java
@@ -0,0 +1,12 @@
+package com.ymagis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDuMaterielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDuMaterielApplication.class, args);
+	}
+}
