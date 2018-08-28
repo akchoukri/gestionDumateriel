39c88fff8b800f527da90af6b8ae49b706817e77
akchoukri <achoukri@ymagisgroup.com>
2018-08-28 16:18:21 +0100

    gestion des client

---
+++ src/main/java/com/ymagis/dao/ClientRepository.java
@@ -0,0 +1,19 @@
+package com.ymagis.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ymagis.model.Client;
+
+
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+	
+	@Query("select c from Client c where c.nomClient like :x ORDER BY c.idClient ASC")
+	public Page<Client> getClientByNom(@Param("x")String mc, Pageable pageable);
+}
---
+++ src/main/resources/static/js/client/controller/updateClient.controller.js
@@ -0,0 +1,39 @@
+(function () {
+
+
+var app = angular.module("myApp");
+app.controller("clientUpdateCtrl",updateClientCtrl);
+
+//controller pour mis a jour client
+function updateClientCtrl($scope,clientDataService,$stateParams,$location,$state) {
+
+
+	$scope.client=null;
+	
+	// faire appel service pour recuperer un client en precisant id
+	clientDataService.getClient($stateParams.id)
+    .then(function (data) {
+    	$scope.client = data;
+    	$scope.client.dateNaissanceClient = new Date($scope.client.dateNaissanceClient);
+        //console.log(data)
+    })
+   
+
+    //mettre a jour client
+    $scope.updateClient= function () {
+		//console.log($scope.client)
+    clientDataService.updateClient($scope.client);
+    	$state.go("clients", {}, {reload: true});
+    };
+    
+    //annuler la mise a jour et redirection a la vue clients
+    $scope.annulerUpdate=function () {
+    	$location.path("clients");
+    }
+
+
+
+    
+}
+
+})();
