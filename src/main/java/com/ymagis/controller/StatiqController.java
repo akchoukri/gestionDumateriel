48d8d14a2c0425fa13c610d0452ba2bf79cd6618
akchoukri <achoukri@ymagisgroup.com>
2018-09-17 14:57:21 +0100

    tt

--- src/main/java/com/ymagis/controller/StatiqController.java
+++ src/main/java/com/ymagis/controller/StatiqController.java
@@ -1,9 +1,45 @@
 package com.ymagis.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ymagis.dao.EmpruntRepository;
+import com.ymagis.model.Emprunt;
 
 @RestController
 public class StatiqController {
 	
+	@Autowired
+	private EmpruntRepository empruntRepository;
+	
+	// recuperer les 3 mois derrr
+		@RequestMapping(value = "/mois", method = RequestMethod.GET)
+		public List<Date> getLastMonths(  Date date) {
+			
+			List<Date> list = new ArrayList<>();
+			
+			for(int i=0;i<4;i++)
+				{
+				date = new Date();
+				date.setMonth(date.getMonth()-i);
+				list.add(date);
+				
+				}
+
+			return list;
+		}
+		
+		// emprut
+		@RequestMapping(value = "/nbrEmprun", method = RequestMethod.GET)
+		public int getempruntR(  Date date) {
+			List<Emprunt> emprunts = empruntRepository.getEmpruntRetour();
+     return emprunts.size();
+		}
 	
 }
--- src/main/java/com/ymagis/dao/EmpruntRepository.java
+++ src/main/java/com/ymagis/dao/EmpruntRepository.java
@@ -1,10 +1,18 @@
 package com.ymagis.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ymagis.model.Client;
 import com.ymagis.model.Emprunt;
 
 public interface EmpruntRepository extends JpaRepository<Emprunt, Long> {
 
 	Emprunt findByIdEmpruntLike(Long idEmprunt);
+	
+	@Query("select c from Emprunt c where   EXTRACT(MONTH FROM TIMESTAMP c.dateRetour) = 9")
+	public List<Emprunt> getEmpruntRetour();
 
 }
